a446b0c6ae111cbdaeb55a7448118e999c821629
Kevin Ng <kevinwng23@gmail.com>
2023-01-16 15:57:19 -0500

    adding templates

--- main_app/views.py
+++ main_app/views.py
@@ -7,10 +7,9 @@ from django.http import HttpResponse
 class Home(View):
 
     def get(self, request):
-     
         return HttpResponse("Original G.S Home")
 class About(View):
 
     def get(self, request):
-     
         return HttpResponse("Original G.S About")
+        

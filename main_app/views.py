1f04f363029a821d8b78d323a2f7f356d26b4f87
Kevin Ng <kevinwng23@gmail.com>
2023-01-16 15:59:56 -0500

    added home templateview

--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.views import View 
-from django.http import HttpResponse 
+# from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 
 
 
-class Home(View):
+# class Home(View):
 
-    def get(self, request):
-        return HttpResponse("Original G.S Home")
-class About(View):
+#     def get(self, request):
+#         return HttpResponse("Original G.S Home")
+# class About(View):
 
-    def get(self, request):
-        return HttpResponse("Original G.S About")
+#     def get(self, request):
+#         return HttpResponse("Original G.S About")
+
+class Home(TemplateView):
+    template_name = "home.html"
+# class About(TemplateView):
+#     template_name = "home.html"

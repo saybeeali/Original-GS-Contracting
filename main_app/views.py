393b482cde0fe5c56aa2abd32716e58c9c96f0bb
Kevin Ng <kevinwng23@gmail.com>
2023-01-16 16:31:21 -0500

    made model

--- main_app/views.py
+++ main_app/views.py
@@ -18,3 +18,10 @@ class Home(TemplateView):
     template_name = "home.html"
 class About(TemplateView):
     template_name = "about.html"
+class ProjectList(TemplateView):
+    template_name = "project_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["projects"] = Project.objects.all() # Here we are using the model to query the database for us.
+        return context

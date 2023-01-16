9b567374c4ba54a50457a72c3b5ecfdcdf3acdb7
Kevin Ng <kevinwng23@gmail.com>
2023-01-16 16:36:56 -0500

    imported project model

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View 
 # from django.http import HttpResponse
 from django.views.generic.base import TemplateView
+from .models import Project
 
 
 

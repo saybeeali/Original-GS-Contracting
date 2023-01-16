09e5be614b3fd03565f7deffa884f3ba36f6ccce
Kevin Ng <kevinwng23@gmail.com>
2023-01-16 16:17:52 -0500

    fixed about template

--- main_app/views.py
+++ main_app/views.py
@@ -17,4 +17,4 @@ from django.views.generic.base import TemplateView
 class Home(TemplateView):
     template_name = "home.html"
 class About(TemplateView):
-    template_name = "home.html"
+    template_name = "about.html"

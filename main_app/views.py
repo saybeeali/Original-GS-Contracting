5aff291da231aa89d0dabd3d26724dd53b607f98
Kevin Ng <kevinwng23@gmail.com>
2023-01-17 14:51:00 -0500

    added dynamic search

--- main_app/views.py
+++ main_app/views.py
@@ -17,12 +17,13 @@ class ProjectList(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # to get the query parameter we have to access it in the request.GET dictionary object        
         name = self.request.GET.get("name")
-        # If a query exists we will filter by name 
         if name != None:
-            # .filter is the sql WHERE statement and name__icontains is doing a search for any name that contains the query param
             context["projects"] = Project.objects.filter(name__icontains=name)
+            # We add a header context that includes the search param
+            context["header"] = f"Searching for {name}"
         else:
             context["projects"] = Project.objects.all()
+            # default header for not searching 
+            context["header"] = f"Searching for {name}"
         return context

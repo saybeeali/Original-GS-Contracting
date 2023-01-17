c9046fb54ae8602d2dbe853da9026292b8f1cd76
Kevin Ng <kevinwng23@gmail.com>
2023-01-17 14:47:09 -0500

    refactored get_context_data

--- main_app/views.py
+++ main_app/views.py
@@ -17,5 +17,12 @@ class ProjectList(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["projects"] = Project.objects.all() 
+        # to get the query parameter we have to access it in the request.GET dictionary object        
+        name = self.request.GET.get("name")
+        # If a query exists we will filter by name 
+        if name != None:
+            # .filter is the sql WHERE statement and name__icontains is doing a search for any name that contains the query param
+            context["projects"] = Project.objects.filter(name__icontains=name)
+        else:
+            context["projects"] = Project.objects.all()
         return context

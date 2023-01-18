4781cbf0b63890b79df5526ca4fbd82a31118a1e
Kevin Ng <kevinwng23@gmail.com>
2023-01-18 14:43:25 -0500

    updated project_detail template and added Work model

--- main_app/models.py
+++ main_app/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-
     name = models.CharField(max_length=100)
     img = models.CharField(max_length=250)
     description = models.TextField(max_length=500)
@@ -12,3 +11,11 @@ class Project(models.Model):
 
     class Meta:
         ordering = ['name']
+
+class Work(models.Model):
+    name = models.CharField(max_length=100)
+    img = models.CharField(max_length=250)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="work")
+
+    def __str__(self):
+        return self.name
